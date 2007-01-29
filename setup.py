38c6533af55c43c8852ddb56ba0908f9cf82dada
dclinton <dclinton>
2007-01-29 01:53:53 +0000

    List the twitter module by name in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from ez_setup import use_setuptools
 use_setuptools()
 
 import setuptools
-import unittest
 
 setuptools.setup(
   name = "python-twitter",
   version = "0.1",
-  packages = setuptools.find_packages(),
+  py_modules = ['twitter'],
   author='DeWitt Clinton',
   author_email='dewitt@google.com',
   description='A python wrapper around the Twitter API',
@@ -15,5 +14,5 @@ setuptools.setup(
   url='http://code.google.com/p/python-twitter',
   keywords='twitter api',
   install_requires = [ 'simplejson >= 1.5' ],
-  test_suite = 'twitter_test.suite'
+  test_suite = 'twitter_test.suite',
   )
