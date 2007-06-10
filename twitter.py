804c6fa0e40d425b125df19c38178c1ee5c39300
dclinton <dclinton>
2007-06-10 20:17:41 +0000

    Completed the DirectMessage class

--- twitter.py
+++ twitter.py
@@ -553,22 +553,255 @@ class User(object):
                 status=status)
 
 class DirectMessage(object):
+  '''A class representing the DirectMessage structure used by the twitter API.
+
+  The DirectMessage structure exposes the following properties:
+
+    direct_message.id
+    direct_message.created_at
+    direct_message.created_at_in_seconds # read only
+    direct_message.sender_id
+    direct_message.sender_screen_name
+    direct_message.recipient_id
+    direct_message.recipient_screen_name
+    direct_message.text
+  '''
 
   def __init__(self,
-               created_at=None,
-               recipient_id=None,
-               sender_id=None,
-               text=None,
-               sender_screen_name=None,
                id=None,
-               recipient_screen_name=None):
-    self.created_at = created_at
-    self.recipient_id = recipient_id
-    self.sender_id = sender_id
-    self.text = text
-    self.sender_screen_name = sender_screen_name
+               created_at=None,
+               sender_id=None,
+               sender_screen_name=None,
+               recipient_id=None,
+               recipient_screen_name=None,
+               text=None):
+    '''An object to hold a Twitter direct message.
+
+    This class is normally instantiated by the twitter.Api class and
+    returned in a sequence.
+
+    Note: Dates are posted in the form "Sat Jan 27 04:17:38 +0000 2007"
+
+    Args:
+      id: The unique id of this direct message
+      created_at: The time this direct message was posted
+      sender_id: The id of the twitter user that sent this message
+      sender_screen_name: The name of the twitter user that sent this message
+      recipient_id: The id of the twitter that received this message
+      recipient_screen_name: The name of the twitter that received this message
+      text: The text of this direct message
+    '''
     self.id = id
+    self.created_at = created_at
+    self.sender_id = sender_id
+    self.sender_screen_name = sender_screen_name
+    self.recipient_id = recipient_id
     self.recipient_screen_name = recipient_screen_name
+    self.text = text
+
+  def GetId(self):
+    '''Get the unique id of this direct message.
+
+    Returns:
+      The unique id of this direct message
+    '''
+    return self._id
+
+  def SetId(self, id):
+    '''Set the unique id of this direct message.
+
+    Args:
+      id: The unique id of this direct message
+    '''
+    self._id = id
+
+  id = property(GetId, SetId,
+                doc='The unique id of this direct message.')
+
+  def GetCreatedAt(self):
+    '''Get the time this direct message was posted.
+
+    Returns:
+      The time this direct message was posted
+    '''
+    return self._created_at
+
+  def SetCreatedAt(self, created_at):
+    '''Set the time this direct message was posted.
+
+    Args:
+      created_at: The time this direct message was created
+    '''
+    self._created_at = created_at
+
+  created_at = property(GetCreatedAt, SetCreatedAt,
+                        doc='The time this direct message was posted.')
+
+  def GetCreatedAtInSeconds(self):
+    '''Get the time this direct message was posted, in seconds since the epoch.
+
+    Returns:
+      The time this direct message was posted, in seconds since the epoch.
+    '''
+    return time.mktime(time.strptime(self.created_at, '%a %b %d %H:%M:%S +0000 %Y'))
+
+  created_at_in_seconds = property(GetCreatedAtInSeconds,
+                                   doc="The time this direct message was "
+                                       "posted, in seconds since the epoch")
+
+  def GetSenderId(self):
+    '''Get the unique sender id of this direct message.
+
+    Returns:
+      The unique sender id of this direct message
+    '''
+    return self._sender_id
+
+  def SetSenderId(self, sender_id):
+    '''Set the unique sender id of this direct message.
+
+    Args:
+      sender id: The unique sender id of this direct message
+    '''
+    self._sender_id = sender_id
+
+  sender_id = property(GetSenderId, SetSenderId,
+                doc='The unique sender id of this direct message.')
+
+  def GetSenderScreenName(self):
+    '''Get the unique sender screen name of this direct message.
+
+    Returns:
+      The unique sender screen name of this direct message
+    '''
+    return self._sender_screen_name
+
+  def SetSenderScreenName(self, sender_screen_name):
+    '''Set the unique sender screen name of this direct message.
+
+    Args:
+      sender_screen_name: The unique sender screen name of this direct message
+    '''
+    self._sender_screen_name = sender_screen_name
+
+  sender_screen_name = property(GetSenderScreenName, SetSenderScreenName,
+                doc='The unique sender screen name of this direct message.')
+
+  def GetRecipientId(self):
+    '''Get the unique recipient id of this direct message.
+
+    Returns:
+      The unique recipient id of this direct message
+    '''
+    return self._recipient_id
+
+  def SetRecipientId(self, recipient_id):
+    '''Set the unique recipient id of this direct message.
+
+    Args:
+      recipient id: The unique recipient id of this direct message
+    '''
+    self._recipient_id = recipient_id
+
+  recipient_id = property(GetRecipientId, SetRecipientId,
+                doc='The unique recipient id of this direct message.')
+
+  def GetRecipientScreenName(self):
+    '''Get the unique recipient screen name of this direct message.
+
+    Returns:
+      The unique recipient screen name of this direct message
+    '''
+    return self._recipient_screen_name
+
+  def SetRecipientScreenName(self, recipient_screen_name):
+    '''Set the unique recipient screen name of this direct message.
+
+    Args:
+      recipient_screen_name: The unique recipient screen name of this direct message
+    '''
+    self._recipient_screen_name = recipient_screen_name
+
+  recipient_screen_name = property(GetRecipientScreenName, SetRecipientScreenName,
+                doc='The unique recipient screen name of this direct message.')
+
+  def GetText(self):
+    '''Get the text of this direct message.
+
+    Returns:
+      The text of this direct message.
+    '''
+    return self._text
+
+  def SetText(self, text):
+    '''Set the text of this direct message.
+
+    Args:
+      text: The text of this direct message
+    '''
+    self._text = text
+
+  text = property(GetText, SetText,
+                  doc='The text of this direct message')
+
+  def __ne__(self, other):
+    return not self.__eq__(other)
+
+  def __eq__(self, other):
+    try:
+      return other and \
+          self.id == other.id and \
+          self.created_at == other.created_at and \
+          self.sender_id == other.sender_id and \
+          self.sender_screen_name == other.sender_screen_name and \
+          self.recipient_id == other.recipient_id and \
+          self.recipient_screen_name == other.recipient_screen_name and \
+          self.text == other.text
+    except AttributeError:
+      return False
+
+  def __str__(self):
+    '''A string representation of this twitter.DirectMessage instance.
+
+    The return value is the same as the JSON string representation.
+
+    Returns:
+      A string representation of this twitter.DirectMessage instance.
+    '''
+    return self.AsJsonString()
+
+  def AsJsonString(self):
+    '''A JSON string representation of this twitter.DirectMessage instance.
+
+    Returns:
+      A JSON string representation of this twitter.DirectMessage instance
+   '''
+    return simplejson.dumps(self.AsDict(), sort_keys=True)
+
+  def AsDict(self):
+    '''A dict representation of this twitter.DirectMessage instance.
+
+    The return value uses the same key names as the JSON representation.
+
+    Return:
+      A dict representing this twitter.DirectMessage instance
+    '''
+    data = {}
+    if self.id:
+      data['id'] = self.id
+    if self.created_at:
+      data['created_at'] = self.created_at
+    if self.sender_id:
+      data['sender_id'] = self.sender_id
+    if self.sender_screen_name:
+      data['sender_screen_name'] = self.sender_screen_name
+    if self.recipient_id:
+      data['recipient_id'] = self.recipient_id
+    if self.recipient_screen_name:
+      data['recipient_screen_name'] = self.recipient_screen_name
+    if self.text:
+      data['text'] = self.text
+    return data
 
   @staticmethod
   def NewFromJsonDict(data):
